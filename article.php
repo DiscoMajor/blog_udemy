bf2ddfc60e9ec9096efa1ef6040689e3c19b208e
DiscoMajor <coolstorybrotherz@gmail.com>
2020-05-05 16:28:37 +0200

    Update BLOG Cours udemy

---
+++ article.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Blog Animaux</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="script.js"></script>
+</head>
+
+<!-- HEADER -->
+<header>
+
+    <nav>
+        <ul>
+            <li><a href="/">Acceuil</a></li>
+        </ul>
+    </nav>
+
+    <h1>BLOG ADOPTIONS</h1>
+
+</header>
+
+<body>
+
+
+
+    ...
+    <!-- Le reste du contenu -->
+    ...
+
+    <!--FOOTER -->
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
