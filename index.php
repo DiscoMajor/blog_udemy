71b527d45eb91967cf1a77392d0b45131a95bfce
DiscoMajor <coolstorybrotherz@gmail.com>
2020-05-05 20:11:08 +0200

    CSS/HTML avancé, side bar otw

--- index.php
+++ index.php
@@ -20,9 +20,9 @@
 
             <nav>
                 <ul class="ul_display">
-                    <li><img src="img/heart.png" alt=""><a href="/" class="a_nav">Acceuil</a></li>
-                    <li><img src="img/cat.png" alt=""><a href="" class="a_nav">Chats</a></li>
-                    <li><img src="img/dog.png" alt=""><a href="" class="a_nav">Chiens</a></li>
+                    <li class="li_navheaderfooter"><img src="img/heart.png" alt=""><a href="/" class="a_nav">Acceuil</a></li>
+                    <li class="li_navheaderfooter"><img src="img/cat.png" alt=""><a href="" class="a_nav">Chats</a></li>
+                    <li class="li_navheaderfooter"><img src="img/dog.png" alt=""><a href="" class="a_nav">Chiens</a></li>
                 </ul>
             </nav>
 
@@ -33,55 +33,78 @@
     <h1>Derniers arrivants et disponibles à l'adoption</h1>
 
 
-    <div class="container">
+    <div class="articles_container ">
 
-        <section class="articles">
+        <div id="flexbox_chat">
+            <section class="articles">
+                <div class="article_section">
+                    <img src="img/inouk.jpeg" alt="miniature">
+                    <h3>INOUK</h3>
+                    <span class="span_text">"La force tranquille"</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus itaque cupiditate nemo in maxime.</p>
+                </div>
+            </section>
 
-            <div class="article">
-                <img src="img/inouk.jpeg" alt="miniature">
-                <h3>INOUK</h3>
-                <span class="span_text">"La force tranquille"</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus itaque cupiditate nemo in maxime.</p>
-            </div>
+
+            <section class="articles">
+                <div class="article_section">
+                    <img src="img/LAKSHMI.jpeg" alt="miniature">
+                    <h3>LAKSHMI</h3>
+                    <span class="span_text">Chat Fougueux</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus itaque cupiditate nemo in maxime.</p>
+                </div>
+            </section>
+        </div>
+
+        <div id="flexbox_chien">
+            <section class="articles">
+
+                <div class="article_section">
+                    <img src="img/POLO.jpg" alt="miniature">
+                    <h3>POLO</h3>
+                    <span class="span_text">Ami des enfants</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus itaque cupiditate nemo in maxime.</p>
+                </div>
+            </section>
+
+
+            <section class="articles">
+                <div class="article_section">
+                    <img src="img/BLACKY.jpg" alt="miniature">
+                    <h3>BLACKY</h3>
+                    <span class="span_text">Compagnon de promenades</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus itaque cupiditate nemo in maxime.</p>
+                </div>
+            </section>
+        </div>
+
+
+
+
+
+        <section class="sidebar">
+            <form class="formsearch">
+                <input type="text" placeholder="Rechercher...">
+                <input type="submit" value="OK">
+            </form>
+
+            <h4>Catégories</h4>
+            <ul>
+                <li>Chiens</li>
+                <li>Chats</li>
+                <li>A propos</li>
+            </ul>
+
+            <h4>Commentaires récents</h4>
+            <ul>
+
+            </ul>
         </section>
-
-
-        <section class="articles">
-
-            <div class="article">
-                <img src="img/LAKSHMI.jpeg" alt="miniature">
-                <h3>LAKSHMI</h3>
-                <span class="span_text">Chat Fougueux</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus itaque cupiditate nemo in maxime.</p>
-            </div>
-        </section>
-
-
-        <section class="articles">
-
-            <div class="article">
-                <img src="img/POLO.jpg" alt="miniature">
-                <h3>POLO</h3>
-                <span class="span_text">Ami des enfants</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus itaque cupiditate nemo in maxime.</p>
-            </div>
-        </section>
-
-
-        <section class="articles">
-
-            <div class="article">
-                <img src="img/BLACKY.jpg" alt="miniature">
-                <h3>BLACKY</h3>
-                <span class="span_text">Compagnon de promenades</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat natus itaque cupiditate nemo in maxime.</p>
-            </div>
-        </section>
-
-
-        <section class="sidebar"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis soluta culpa a in beatae tempora labore necessitatibus eius optio sunt voluptatem nisi illo repellendus velit omnis assumenda inventore, provident ipsum.</p></section>
 
     </div>
+
+
+
 
 
 
